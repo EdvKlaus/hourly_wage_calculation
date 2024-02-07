3c8f152da04762be31aeb6b2f79437386848a287
EdvKlaus <131393806+EdvKlaus@users.noreply.github.com>
2024-02-07 20:22:38 +0300

    Add files via upload

---
+++ payroll_preparation/Calculation.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace payroll_preparation
+{
+    public partial class Calculation : Form
+    {
+        int oplata;
+        public Calculation(int oplata)
+        {
+            InitializeComponent();
+            this.oplata = oplata;
+        }
+
+        private void Calculation_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int number_of_days_worked = Convert.ToInt32(textBox1.Text);
+                int number_of_hours_worked = Convert.ToInt32(textBox2.Text);
+                int hourly_bonus_amount = Convert.ToInt32(textBox3.Text);
+
+                if (number_of_days_worked >= 1 && number_of_hours_worked >= 1 && hourly_bonus_amount >= 0) {
+                    if (number_of_days_worked < 8) {
+                        double weekly_salary = (oplata + hourly_bonus_amount) * number_of_hours_worked * number_of_days_worked;
+                        double two_weeks_salary = (oplata + hourly_bonus_amount) * number_of_hours_worked * (number_of_days_worked * 2);
+                        double mounthly_salary = (oplata + hourly_bonus_amount) * number_of_hours_worked * number_of_days_worked * 4;
+
+                        textBox6.Text = weekly_salary.ToString();
+                        textBox5.Text = two_weeks_salary.ToString();
+                        textBox4.Text = mounthly_salary.ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Число рабочих дней (в неделю) должно быть в диапозоне от 1 до 7", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Все числа должны быть целыми и положительными", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Все поля для расчётов должны быть заполнениы целыми числами", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void Generation_File_Click(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+
+
+            string fileStr = "Текущая дата: " + dateTime.ToString() + "\n" + "Заработная плата за неделю: " + textBox6.Text.ToString() + "\n" + "Заработная плата за 2 неделе: " + textBox5.Text.ToString() + "\n" + "Заработная плата за месяц: " + textBox4.Text.ToString();
+
+
+            if (textBox6.Text == ""|| textBox5.Text == "" || textBox4.Text == "") {
+                MessageBox.Show("Файл не может быть создан без произведения расчётов", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                saveFileDialog1.FileName = @"calculation.txt";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        var writer = new StreamWriter(
+                        saveFileDialog1.FileName, false, Encoding.GetEncoding(1251));
+
+                        writer.Write(fileStr);
+                        writer.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message,
+                            "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                }
+
+            }
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            StartForm startForm = new StartForm();
+            startForm.Show();
+            this.Close();
+        }
+    }
+}
